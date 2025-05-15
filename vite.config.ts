46d738f2a9796b9ee1a4dae8658db098211ced44
troitonn <150198515+troitonn@users.noreply.github.com>
2025-05-15 15:16:30 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    tailwindcss(),
+  ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000', // proxy /api requests to backend
+    },
+  },
+
+
+})
